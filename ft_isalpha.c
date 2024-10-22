5a0af7dd9f5bda99f78f6abd64dd0d59b64ba22a
Ossama Touayri <ostouayr@c3r9p5.1337.ma>
2024-10-22 09:46:17 +0100

    scond

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,16 +6,23 @@
 /*   By: ostouayr <ostouayr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 21:53:08 by ostouayr          #+#    #+#             */
-/*   Updated: 2024/10/21 22:31:09 by ostouayr         ###   ########.fr       */
+/*   Updated: 2024/10/22 09:41:26 by ostouayr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	isalpha(int c)
+#include "libft.h"
+
+int	ft_isalpha(int c)
 {
-	if ((c >= 65 && c <= 90 || (c >= 97 && c <= 122)))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
 	else
 		return (0);
+}
+int main(){
+	char c = 'a';
+	int count = ft_isalpha(c);
+	printf("%d\n",count);
 }
