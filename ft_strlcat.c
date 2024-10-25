aa756252c88a45bab105842cbffe7295eef5198f
root <otouayri@gmail.com>
2024-10-25 01:27:33 +0100

    first

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ostouayr <ostouayr@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 22:07:56 by ostouayr          #+#    #+#             */
-/*   Updated: 2024/10/24 22:44:40 by ostouayr         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "libft.h"
 
@@ -19,11 +8,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 
 	i = 0;
 	j = 0;
+	if(!dst && !size)
+		return(ft_strlen(src));
 	if(size <= ft_strlen(dst))
 		return (size + ft_strlen(src));
 	while (dst[i])
 		i++;
-	while (i < size -1 && (src[j] || dst[i]))
+	while (i < size -1 && src[j])
 	{
 		dst[i] = src[j];
 		i++;
@@ -31,9 +22,4 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	}
 	dst[i] = '\0';
 	return(ft_strlen(dst) + ft_strlen(src));
-}
-int main(){
-	char dest[5] = "he";
-	int count = strlcat(dest,"hello",5);
-	printf("%d\n",count);
 }
