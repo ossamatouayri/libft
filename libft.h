4c08362d62fcc717fe66cb317f38d8d598799583
root <otouayri@gmail.com>
2024-10-26 10:54:08 +0100

    push

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ostouayr <ostouayr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ostouayr <ostouayr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:31:44 by ostouayr          #+#    #+#             */
-/*   Updated: 2024/10/24 22:18:50 by ostouayr         ###   ########.fr       */
+/*   Updated: 2024/10/26 10:36:57 by ostouayr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 size_t ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-
+int ft_memcmp(const void *s1, const void *s2, size_t n);
+char *ft_strnstr(const char *big, const char *little, size_t len);
 #endif
