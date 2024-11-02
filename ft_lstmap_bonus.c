215aeb9e67d1c2d19b096cdf1f70bcdaa714a267
Ossama Touayri <ostouayr@c4r7p7.1337.ma>
2024-11-02 10:28:48 +0100

    push

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ostouayr <ostouayr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 19:01:32 by ostouayr          #+#    #+#             */
-/*   Updated: 2024/11/01 21:55:16 by ostouayr         ###   ########.fr       */
+/*   Updated: 2024/11/02 10:27:25 by ostouayr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list *new_list;
-	t_list *new_content;
+	t_list	*new_list;
+	void	*new_content;
+	t_list	*new_node;
 
-	new_content->content = f(lst->content);
-
+	if (!lst)
+		return (NULL);
+	new_list = NULL;
+	new_node = NULL;
+	while (lst)
+	{
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
